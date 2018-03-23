c790cd31c769761832e6aa88438ed482c16410db
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-22 20:10:09 -0400

    Install mongoose, outline submission schema

---
+++ models/submission.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const submissionSchema = new Schema({
+    name: {
+        required: true,
+        type: String
+    },
+    karmaCount: {
+        required: true,
+        type: Number,
+        default: 0
+    },
+    price: {
+        required: true,
+        type: Number
+    },
+    purchaseType: {
+        required: true,
+        type: String
+    },
+    createTimestamp: {
+        required: true,
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Submission = mongoose.model('Submission', submissionSchema);
+module.export = Submission;
