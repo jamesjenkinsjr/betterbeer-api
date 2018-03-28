9739b6b853dabf309d4b41bfce93f5a0fab40b67
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-28 18:56:28 -0400

    Install and use cors in index for serverApp

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const serverApp = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -6,6 +7,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 const submissionRouter = require('./routers/submissions');
 serverApp.use(bodyParser.json());
+serverApp.use(cors());
 serverApp.use(bodyParser.urlencoded({extended: true}));
 serverApp.use(submissionRouter);
 mongoose.connect(process.env.MONGO_URI);
@@ -13,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI);
 
 
 const PORT = process.env.PORT || 5000;
-serverApp.get('/', (req, res) => {
+serverApp.get('/', cors(), (req, res) => {
     res.send('I work Fam!');
 });
 
