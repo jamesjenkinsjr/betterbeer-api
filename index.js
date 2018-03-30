4c778769d6a9354bac86b5231a9ccb985af35fc2
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-29 20:50:33 -0400

    Trying more stuff to debug

--- index.js
+++ index.js
@@ -10,16 +10,23 @@ serverApp.use(bodyParser.json());
 serverApp.use(cors());
 serverApp.use(bodyParser.urlencoded({extended: true}));
 serverApp.use(submissionRouter);
+serverApp.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 mongoose.connect(process.env.MONGO_URI);
 
 
 
 const PORT = process.env.PORT || 5000;
-serverApp.get('/', cors(), (req, res) => {
+serverApp.get('/', cors(), (req, res, next) => {
     res.send('I work Fam!');
+    next();
 });
-serverApp.post('/submissions', cors(), (req, res) => {
+serverApp.post('/submissions', cors(), (req, res, next) => {
     res.send('I work Fam!');
+    next();
 });
 serverApp.listen(PORT, () => {
     console.log('Now listening on port 5000');
