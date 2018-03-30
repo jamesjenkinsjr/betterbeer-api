d2f2339b025a21dfcb65277640aed2083d8b7baf
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-29 20:55:13 -0400

    Put back potential, non-function fix for res.header

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ serverApp.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-})
+});
 mongoose.connect(process.env.MONGO_URI);
 
 
